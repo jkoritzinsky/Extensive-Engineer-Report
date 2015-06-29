2a7b72dc802273c69c9fe34095f022ebee099d64
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-06-28 21:13:48 -0500

    Added SAS check.

---
+++ HasSASModuleOrPilot.cs
@@ -0,0 +1,33 @@
+﻿using PreFlightTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JKorTech.Extensive_Engineer_Report
+{
+    public class HasSASModuleOrPilot : DesignConcernBase
+    {
+        public override string GetConcernDescription()
+        {
+            return "Vessel lacks pilots and SAS modules. Control might be difficult.";
+        }
+
+        public override string GetConcernTitle()
+        {
+            return "No SAS";
+        }
+
+        public override DesignConcernSeverity GetSeverity()
+        {
+            return DesignConcernSeverity.NOTICE;
+        }
+
+        public override bool TestCondition()
+        {
+            var hasPilots = ShipConstruction.ShipManifest.GetAllCrew(false).Any(crew => crew.experienceTrait.TypeName == "Pilot");
+            var hasSAS = EditorLogic.SortedShipList.Any(part => part.FindModuleImplementing<ModuleSAS>() != null);
+            return hasPilots || hasSAS;
+        }
+    }
+}
