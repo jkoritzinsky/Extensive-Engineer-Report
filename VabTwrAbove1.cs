5718743b4b3967ec237e4da415418f6b769bf4b0
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-07-22 19:57:42 -0500

    Removing debug code.

--- VabTwrAbove1.cs
+++ VabTwrAbove1.cs
@@ -27,22 +27,10 @@ namespace JKorTech.Extensive_Engineer_Report
         {
             var mass = EditorLogic.SortedShipList.Where(part => part.FindModuleImplementing<LaunchClamp>() == null).Sum(part => part.mass * 9.81);
             var firstStage = EditorLogic.SortedShipList.Max(part => part.inverseStage);
-            var thrust = EditorLogic.SortedShipList.Where(part => part.inverseStage == firstStage)
-                                                    .SelectMany(part => part.FindModulesImplementing<ModuleEngines>())
-                                                    .Sum(engine => engine.GetMaxThrust());
             var thrustByStage = EditorLogic.SortedShipList.GroupBy(part => part.inverseStage)
                                                     .OrderByDescending(group => group.Key)
                                                     .Select(group => group.SelectMany(part => part.FindModulesImplementing<ModuleEngines>())
                                                                            .Select(engine => engine.GetMaxThrust()).Sum());
-            thrustByStage = thrustByStage.ToList();
-            foreach (var stageThrust in thrustByStage)
-            {
-                UnityEngine.Debug.Log("[EER] Stage thrust pre filter: " + stageThrust);
-            }
-            foreach (var stageThrust in thrustByStage.SkipWhile(stage => stage < .001))
-            {
-                UnityEngine.Debug.Log("[EER] Stage thrust post filter: " + stageThrust);
-            }
             var firstEngineStageThrust = thrustByStage.FirstOrDefault(stageThrust => stageThrust > .001);
             UnityEngine.Debug.Log("[EER] " + firstEngineStageThrust * 1000 + "<>" + mass);
             return firstEngineStageThrust > mass;
