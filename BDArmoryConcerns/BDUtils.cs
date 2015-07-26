ca8db0738d4a435061b28a7a8855f3b22fb8171e
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-07-26 18:01:14 -0500

    Fixed up remaining issues from changing framework version.

--- BDArmoryConcerns/BDUtils.cs
+++ BDArmoryConcerns/BDUtils.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using JKorTech.Extensive_Engineer_Report;
 
 namespace BDArmoryConcerns
