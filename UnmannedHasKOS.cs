e848593604bb12d925c92419b9803f5215ae43c1
Jeremy Koritzinsky <jkoritzinsky@live.com>
2015-07-26 17:59:34 -0500

    Add Unmanned kOS test.

---
+++ UnmannedHasKOS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JKorTech.Extensive_Engineer_Report
+{
+    public class UnmannedHasKOS : SectionDesignConcernBase
+    {
+        public override bool TestCondition(IEnumerable<Part> sectionParts)
+        {
+            if (!AssemblyLoader.loadedAssemblies.Any(assembly => assembly.assembly.GetName().Name == "kOS"))
+                return true;
+            var manned = CrewInSection(sectionParts).Any(pair => pair.Value.HasModule<ModuleCommand>());
+            return !manned || sectionParts.Any(part => part.Modules["kOSProcessor"]);
+        }
+
+        public override string GetConcernDescription()
+        {
+            return "Your unmanned probe is lacking a kOS processor.";
+        }
+
+        public override string GetConcernTitle()
+        {
+            return "Unmanned Probe and no kOS Processor";
+        }
+
+        public override PreFlightTests.DesignConcernSeverity GetSeverity()
+        {
+            return PreFlightTests.DesignConcernSeverity.NOTICE;
+        }
+    }
+}
