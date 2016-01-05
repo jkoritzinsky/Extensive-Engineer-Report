ebc13aa0b430715d3a0c6a533e9d5a39f4ca6133
Jeremy Koritzinsky <jkoritzinsky@live.com>
2016-01-04 22:09:30 -0600

    Fixed precondition for SuggestFixedPowerGen test.

--- SuggestFixedPowerGenIfOnlyDeployable.cs
+++ SuggestFixedPowerGenIfOnlyDeployable.cs
@@ -23,7 +23,8 @@ namespace JKorTech.Extensive_Engineer_Report
 
         protected internal override bool IsApplicable(IEnumerable<Part> sectionParts)
         {
-            return sectionParts.SelectMany(part => part.FindModulesImplementing<ModuleDeployableSolarPanel>()).All(panel => panel.sunTracking);
+            return sectionParts.SelectMany(part => part.FindModulesImplementing<ModuleDeployableSolarPanel>()).All(panel => panel.sunTracking)
+                 || !sectionParts.AnyHasModule<ModuleDeployableSolarPanel>();
         }
 
         public override bool TestCondition(IEnumerable<Part> sectionParts)
